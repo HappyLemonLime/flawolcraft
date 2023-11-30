be3c19f3d6db67865d5a0dd430b34db0a2d1f154
LemonLime <111824263+HappyLemonLime@users.noreply.github.com>
2023-11-30 10:50:45 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+      var topBar = document.querySelector(".top-bar");
+      topBar.classList.add("visible");
+    }, 500);
+  });
+  
