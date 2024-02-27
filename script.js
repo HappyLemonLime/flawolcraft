085bba9d4aac906cba360a8b183a5b8ff4083489
LemonLime <111824263+HappyLemonLime@users.noreply.github.com>
2024-02-27 08:19:48 -0800

    Delete script.js
    Restarting the website from scratch

--- script.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("DOMContentLoaded", function() {
-    setTimeout(function() {
-      var topBar = document.querySelector(".top-bar");
-      topBar.classList.add("visible");
-    }, 500);
-  });
-  
